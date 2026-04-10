343f189675dad982dfdeff5ed49a31a4c0823360
D <diego22.tec@gmail.com>
2026-04-09 18:15:39 -0600

    Correcion con el script de PS para construir el ejecutable

--- build.ps1
+++ build.ps1
@@ -1,17 +1,17 @@
 Write-Host "builder>> Compilando el proyecto . . ."
-g++ ./src/main.cpp ./src/screen.cpp -o ./out/main.exe
+g++ ./src/dasm.cpp ./src/inputArgs.cpp ./src/adminFile.cpp ./src/programController.cpp -o ./out/dasm.exe
 
 if ($LASTEXITCODE -eq 0) {
     Write-Host "builder>> El proyecto se ha compilado exitosamente!" -ForegroundColor Green
-    Write-Host "builder>> El ejecutable se encuentra en: ./out/main.exe"
+    Write-Host "builder>> El ejecutable se encuentra en: ./out/dasm.exe"
     Write-Host "builder>> Ejecutando el programa . . ." -ForegroundColor Cyan
     Write-Host "=====================================================================" 
 
-    Start-Process -FilePath "./out/main.exe" -NoNewWindow -Wait
+    Start-Process -FilePath "./out/dasm.exe --help" -NoNewWindow -Wait
 }
 else {
     Write-Host "builder>> Error al compilar el proyecto." -ForegroundColor Red
     # Mostrar los errores de compilación
-        g++ ./src/main.cpp ./src/screen.cpp -o ./out/main.exe 2>&1 | ForEach-Object { Write-Host $_ -ForegroundColor Red }
+        g++ ./src/dasm.cpp ./src/inputArgs.cpp ./src/adminFile.cpp ./src/programController.cpp -o ./out/dasm.exe 2>&1 | ForEach-Object { Write-Host $_ -ForegroundColor Red }
     Write-Host "builder>> Por favor, corrige los errores y vuelve a intentarlo." -ForegroundColor Gray
 }
